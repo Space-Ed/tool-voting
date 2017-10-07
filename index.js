2a70c7ab80136a1bc2aeaf1dc923e5ad49833133
Edward <endalley11@gmail.com>
2017-10-07 17:45:12 +1300

    upgrade to static website

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+app.use(express.static('www'))
+
 app.get('/', (req, res) => {
-    res.send('Hello world\n');
+    res.sendFile('www/index.html')
 });
 
 app.listen(PORT, HOST);
+
 console.log(`Running on http://${HOST}:${PORT}`);
